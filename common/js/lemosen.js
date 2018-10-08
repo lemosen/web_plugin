df0babd7847400fd119ea58febe651003baae68d
lemosen <287462961@qq.com>
2018-10-08 16:05:33 +0800

    init common

--- common/js/lemosen.js
+++ common/js/lemosen.js
@@ -1,6 +1,21 @@
-function test() {
-    alert("test")
-}
-Lemosen=function test() {
-    alert("test")
-}
+/**
+ * Copyright (c) 2018 lemosen
+ * email : 28462961@qq.com
+ */
+(function () {
+    'use strict'
+    window.lemosen = (function () {
+        window.lemosen.prototype = {
+            screenWidth: window.screen.width,
+            screenHeight: window.screen.height
+        };
+        return {
+            alert: function () {
+                alert("test");
+            },
+            toast: function () {
+
+            }
+        }
+    })
+});
