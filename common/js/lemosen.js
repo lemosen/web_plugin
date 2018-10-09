f5b10fce1c3daea92496e087068d42ff21c22fca
lemosen <287462961@qq.com>
2018-10-09 16:56:13 +0800

    modal alert

--- common/js/lemosen.js
+++ common/js/lemosen.js
@@ -3,6 +3,73 @@
  * email : 28462961@qq.com
  */
 (function () {
+    window.lemosenCore = (function () {
+            return {
+                prototype: {
+                    screenWidth: window.screen.width,
+                    screenHeight: window.screen.height,
+                    sureCallBackParam: {},
+                    cancelCallBackParam: {},
+                    modalDismiss: {},
+                    modalParam: undefined
+                },
+                saveCallBackParam(param, isSure) {
+                    if (param && param !== {}) {
+                        isSure ? window.lemosenCore.prototype.sureCallBackParam = param
+                            : window.lemosenCore.prototype.cancelCallBackParam = param
+                    }
+                },
+                createDocument: function (htmlDivElement) {
+                    let documentFragment = document.createDocumentFragment();
+                    documentFragment.appendChild(htmlDivElement);
+                    document.body.appendChild(documentFragment)
+                },
+                initPopup: function () {
+                    lemosenCore.saveCallBackParam(arguments[2] ? arguments[2] : {}, true);
+                    lemosenCore.saveCallBackParam(arguments[4] ? arguments[4] : {}, false);
+
+                    let htmlDivElement = document.createElement('div');
+                    htmlDivElement.classList.add(['lemosen-popup'])
+                    return htmlDivElement
+                },
+                alertSure: function (fun) {
+                    if (window.lemosenCore.prototype.sureCallBackParam) {
+                        fun(window.lemosenCore.prototype.sureCallBackParam)
+                    }
+                    document.getElementsByTagName('body').item(0).removeChild(document.getElementsByClassName('lemosen-popup').item(0))
+                },
+                alertCancel: function (fun) {
+                    if (window.lemosenCore.prototype.cancelCallBackParam) {
+                        fun(window.lemosenCore.prototype.cancelCallBackParam)
+                    }
+                    document.getElementsByTagName('body').item(0).removeChild(document.getElementsByClassName('lemosen-popup').item(0))
+                },
+                /**
+                 * 阻止点击模态框引起关闭事件
+                 * @param event
+                 */
+                popupBodyClick: function (event) {
+                    event.stopPropagation();
+                },
+                modalClose: function () {
+                    if (this.prototype.modalParam) {
+                        this.prototype.modalDismiss(this.prototype.modalParam)
+                    }
+                    document.getElementsByTagName('body').item(0).removeChild(document.getElementsByClassName('lemosen-popup').item(0))
+                },
+                modalDismiss: function () {
+
+                },
+                setModalParam: function () {
+                    if (arguments.length !== 0) {
+                        lemosenCore.prototype.modalParam = arguments
+                    }
+                }
+            }
+        }
+    )()
+})();
+(function () {
     window.lemosen = (function () {
         return {
 
@@ -45,84 +112,30 @@
             /**
              * arguments
              * 0 content                require
-             * 1 sureCallBack           require
-             * 2 sureCallBackParam
-             * 3 cancelCallBack
-             * 4 cancelCallBackParam
-             * 5 width      vw
+             * 1 dismiss
+             * 2 width      vw
              */
             modal: function () {
-                if (arguments[3] === undefined) {
-                    arguments[3] = function () {
+                if (arguments[1] === undefined) {
+                    arguments[1] = function () {
                     }
                 }
-                let htmlDivElement = lemosenCore.initPopup()
+                let width = arguments[2] ? arguments[2] + '%' : 35 + '%';
+                let htmlDivElement = lemosenCore.initPopup();
 
                 htmlDivElement.addEventListener("click", htmlDivElement.addEventListener("click", function () {
-                    document.getElementsByTagName('body').item(0).removeChild(document.getElementsByClassName('lemosen-popup').item(0))
+                    lemosenCore.modalClose()
                 }))
 
-                htmlDivElement.innerHTML = '<div onclick="lemosenCore.popupBodyClick(event)" class="lemosen-popup-body" style="width: ' + arguments[5] ? arguments[5] + '' : 35 + '' + '">' +
+                htmlDivElement.innerHTML = '<div onclick="lemosenCore.popupBodyClick(event)" class="lemosen-popup-body" style="width: ' + width + '">' +
                     // '<div class="lemosen-popup-head"><span class="lemosen-popup-close"  onclick="lemosenCore.modalClose()">X</span></div>' +
                     '<div class="lemosen-popup-content">' + arguments[0] + '</div>' +
                     '</div>'
                 lemosenCore.createDocument(htmlDivElement)
             },
 
+
         };
     })()
 })();
-(function () {
-    window.lemosenCore = (function () {
-            return {
-                prototype: {
-                    screenWidth: window.screen.width,
-                    screenHeight: window.screen.height,
-                    sureCallBackParam: {},
-                    cancelCallBackParam: {}
-                },
-                saveCallBackParam(param, isSure) {
-                    if (param && param !== {}) {
-                        isSure ? window.lemosenCore.prototype.sureCallBackParam = param
-                            : window.lemosenCore.prototype.cancelCallBackParam = param
-                    }
-                },
-                createDocument: function (htmlDivElement) {
-                    let documentFragment = document.createDocumentFragment();
-                    documentFragment.appendChild(htmlDivElement);
-                    document.body.appendChild(documentFragment)
-                },
-                initPopup: function () {
-                    lemosenCore.saveCallBackParam(arguments[2] ? arguments[2] : {}, true);
-                    lemosenCore.saveCallBackParam(arguments[4] ? arguments[4] : {}, false);
 
-                    let htmlDivElement = document.createElement('div');
-                    htmlDivElement.classList.add(['lemosen-popup'])
-                    return htmlDivElement
-                },
-                alertSure: function (fun) {
-                    if (window.lemosenCore.prototype.sureCallBackParam) {
-                        fun(window.lemosenCore.prototype.sureCallBackParam)
-                    }
-                    document.getElementsByTagName('body').item(0).removeChild(document.getElementsByClassName('lemosen-popup').item(0))
-                },
-                alertCancel: function (fun) {
-                    if (window.lemosenCore.prototype.cancelCallBackParam) {
-                        fun(window.lemosenCore.prototype.cancelCallBackParam)
-                    }
-                    document.getElementsByTagName('body').item(0).removeChild(document.getElementsByClassName('lemosen-popup').item(0))
-                },
-                /**
-                 * 阻止点击模态框引起关闭事件
-                 * @param event
-                 */
-                popupBodyClick: function (event) {
-                    event.stopPropagation();
-                },
-                modalClose: function () {
-                    document.getElementsByTagName('body').item(0).removeChild(document.getElementsByClassName('lemosen-popup').item(0))
-                }
-            }
-        }
-    )()
-})();
