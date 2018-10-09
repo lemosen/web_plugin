3c528fb107e7ffe06929ddbdfceca53f31e179b0
lemosen <287462961@qq.com>
2018-10-09 20:54:16 +0800

    modal alert

--- common/js/lemosen.js
+++ common/js/lemosen.js
@@ -14,7 +14,7 @@
             alert1: function () {
                 arguments[5] = '提示'
                 // document.body.innerHTML += "<div class=\"lemosen-popup-body\"><div class=\"lemosen-popup-head\">提示</div><div class=\"lemosen-popup-content\">" + arguments[0] + "</div></div>"
-                document.body.innerHTML += '<div class="lemosen-popup-body"><div class="lemosen-popup-head">' + arguments[5] + '</div><div class="lemosen-popup-content">' + arguments[0] + '</div></div>'
+                document.body.innerHTML += '<div class="lemosen-popup-body" onclick="this.alert2()"><div class="lemosen-popup-head">' + arguments[5] + '</div><div class="lemosen-popup-content">' + arguments[0] + '</div></div>'
                 this.alert2()
             },
             alert2:function(){
