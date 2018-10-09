5b363671a69c9b4e8ecf87275e7b8da044132195
lemosen <287462961@qq.com>
2018-10-09 20:48:47 +0800

    modal alert

--- common/js/lemosen.js
+++ common/js/lemosen.js
@@ -3,8 +3,9 @@
     window.lemosen = (function () {
         return {
             alert1: function () {
+                arguments[5] = '提示'
                 // document.body.innerHTML += "<div class=\"lemosen-popup-body\"><div class=\"lemosen-popup-head\">提示</div><div class=\"lemosen-popup-content\">" + arguments[0] + "</div></div>"
-                document.body.innerHTML += '<div class="lemosen-popup-body"><div class="lemosen-popup-head">提示</div><div class="lemosen-popup-content">' + arguments[0] + '</div></div>'
+                document.body.innerHTML += '<div class="lemosen-popup-body"><div class="lemosen-popup-head">'+arguments[5]+'</div><div class="lemosen-popup-content">' + arguments[0] + '</div></div>'
             }
         }
     })()
