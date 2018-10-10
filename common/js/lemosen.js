80ac8e4ec933fc9c0ba121662aa4b9b9000bcccf
lemosen <287462961@qq.com>
2018-10-10 20:49:02 +0800

    modal alert

--- common/js/lemosen.js
+++ common/js/lemosen.js
@@ -23,60 +23,40 @@
                 event.stopPropagation();
             },
             /**
-             * 数据格式
-             * {
-             * top:100, //框里顶部的距离
-             * title: "tip",
-             * content: "alert test ",
-             * okText: "ok",
-             * cancelText: "cancel",
-             * callback: function (e) {
-             *     alert("i am callback")
-             * }
-             * }
+             * arguments
+             * 0 content                require
+             * 1 sureCallBack           require
+             * 2 title
+             * 3 sureButtonText
+             * 4 cancelButtonText
+             * 5 width
              */
             alert: function () {
                 if (!this.isOnlyPopup()) {
                     this.toast();
                     return
                 }
-                if (arguments[0]['callback'] === undefined) {
-                    arguments[0]['callback'] = function () {
 
-                    }
+                if (arguments[3] === undefined) {
+                    arguments[3] = '确定'
                 }
-                if (arguments[0]['title'] === undefined) {
-                    arguments[0]['title'] = '提示'
-                }
-                if (arguments[0]['content'] === undefined) {
-                    arguments[0]['content'] = '无内容'
-                }
-                if (arguments[0]['okText'] === undefined) {
-                    arguments[0]['okText'] = '确定'
-                }
-                if (arguments[0]['cancelText'] === undefined) {
-                    arguments[0]['cancelText'] = '取消'
-                }
-                if (arguments[0]['top'] === undefined) {
-                    arguments[0]['top'] = 50
+                if (arguments[4] === undefined) {
+                    arguments[4] = '取消'
                 }
                 this.prototype.successCallBack = arguments[0]['callback']
+                // let width = arguments[5] ? arguments[5] + '%' : 35 + '%';
                 document.body.innerHTML +=
-                    '<div class="lemosen-popup" >' +
+                    '<div class="lemosen-popup" style="padding-top: 50%;">' +
                     '<div class="lemosen-popup-body" onclick="lemosen.stopCloseEvent(event)">' +
-                    '<div class="lemosen-popup-head">' + arguments[0]['title'] + '</div>' +
+                    '<div class="lemosen-popup-head">' + arguments[0]['title'] ? arguments[0]['title'] : '提示' + '</div>' +
                     '<div class="lemosen-popup-content">' + arguments[0]['content'] + '</div>' +
                     '<div class="lemosen-popup-buttons">' +
-                    '<button class="lemosen-popup-button" onclick="lemosen.popupCallback(true)">' +
-                    '<span class="lemosen-popup-sure-text lemosen-popup-text">' + arguments[0]['okText'] + '</span>' +
-                    '</button>' +
-                    // '<span class="lemosen-popup-button-divide"></span>' +
-                    '<button class="lemosen-popup-button " onclick="lemosen.popupCallback(false)">' +
-                    '<span class="lemosen-popup-cancel-text lemosen-popup-text">' + arguments[0]['cancelText'] + '</span>' +
-                    '</button>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>'
+                    '<button class="lemosen-popup-button lemosen-popup-sure-button" onclick="lemosen.popupCallback(true)">' + arguments[0]['okText'] ? arguments[0]['okText'] : '确定' + '</button>' +
+                    '<span class="lemosen-popup-button-divide"></span>' +
+                    '<button class="lemosen-popup-button lemosen-popup-cancel-button" onclick="lemosen.popupCallback(false)">' + arguments[0]['cancelText'] ? arguments[0]['cancelText'] : '取消' + '</button>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
                 document.getElementsByClassName('lemosen-popup').item(0).addEventListener("click", function () {
                     lemosen.popupCallback(false)
                 })
